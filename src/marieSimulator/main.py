3d82a77693ea9ea90a3e74a59e2878bf679e244c
dehadeaaryan <dehadeaaryan@gmail.com>
2023-03-31 12:51:27 -0500

    1.0.1

--- src/marieSimulator/main.py
+++ src/marieSimulator/main.py
@@ -1,7 +1,7 @@
-from MarieReader import MarieReader
+
 
 class Marie:
-    def __init__(self, mr: MarieReader = None):
+    def __init__(self, mr = None):
         self.AC = 0x0000
         self.PC = 0b000000000000
         self.MAR = 0b000000000000
@@ -18,7 +18,7 @@ class Marie:
         if mr != None:
             self.parse(mr)
     
-    def parse(self, mr: MarieReader):
+    def parse(self, mr):
         self.M = mr.M
         self.symbolTable = mr.symbolTable
 
@@ -156,3 +156,109 @@ class Marie:
         self.MAR = self.MBR
         self.MBR = self.AC
         self.M[self.MAR] = self.MBR
+
+
+
+class MarieReader:
+    def __init__(self, filename):
+        self.input = []
+        self.M = []
+        self.symbolTable = {}
+
+        self.read(filename)
+    
+    def read(self, filename):
+        with open(filename, 'r') as file:
+            for line in file:
+                self.input.append(line)
+                if line != '\n':
+                    self.M.append(0)
+        
+        self.interpret()
+        for item in self.M:
+            if item & 0b100000000000 == 0b100000000000:
+                self.interpret()
+                break
+
+    def interpret(self):
+        ctr = 0
+        for line in self.input:
+            if line[-1] == '\n':
+                line = line[:-1]
+
+            line.strip()
+
+            while line.find('  ') != -1:
+                line.replace('  ', ' ')
+            
+            tokens = line.split(' ') 
+
+            if tokens[0][-1] == ',':
+                self.symbolTable[tokens[0][:-1]] = ctr
+                tokens = tokens[1:]
+
+            opcode = None
+
+            instruction = (tokens[0]).upper()
+            if instruction == 'JNS':
+                opcode = 0xF
+            elif instruction == 'LOAD':
+                opcode = 0x1
+            elif instruction == 'STORE':
+                opcode = 0x2
+            elif instruction == 'ADD':
+                opcode = 0x3
+            elif instruction == 'SUBT':
+                opcode = 0x4
+            elif instruction == 'INPUT':
+                opcode = 0x5
+                tokens.append('000')
+            elif instruction == 'OUTPUT':
+                opcode = 0x6
+                tokens.append('000')
+            elif instruction == 'HALT':
+                opcode = 0x7
+                tokens.append('000')
+            elif instruction == 'SKIPCOND':
+                opcode = 0x8
+            elif instruction == 'JUMP':
+                opcode = 0x9
+            elif instruction == 'CLEAR':
+                opcode = 0xA
+                tokens.append('000')
+            elif instruction == 'ADDI':
+                opcode = 0xB
+            elif instruction == 'JUMPI':
+                opcode = 0xC
+            elif instruction == 'LOADI':
+                opcode = 0xD
+            elif instruction == 'STOREI':
+                opcode = 0xE
+            else:
+                opcode = 0x0
+
+            address = tokens[1]
+            try:
+                address = int(address, 16)
+            except:
+                if address in self.symbolTable:
+                    address = self.symbolTable[address]
+                else:
+                    address = -1
+            
+            self.M[ctr] = (opcode << 12) | address
+
+            ctr += 1
+
+
+    
+
+
+
+def main():
+    marie = Marie(MarieReader('trial'))
+    marie.show()
+    marie.run()
+    marie.show()
+
+main()
